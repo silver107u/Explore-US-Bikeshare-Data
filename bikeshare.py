7810b17ebb24d1de9e5ba571bf552477e9f74710
SilverInUniverse <ayushgarg9752@gmail.com>
2020-05-03 22:35:02 +0530

    refactor: add docstring in raw_data function
    This commit adds a docstring to the function raw_data() in bikeshare.py file. The docstringdescribes what the function does.

--- bikeshare.py
+++ bikeshare.py
@@ -52,7 +52,16 @@ dfjksdoiofj
 
 
 def load_data(city, month, day):
+    """
+        Loads data for the specified city and filters by month and day if applicable.
 
+        Args:
+            (str) city - name of the city to analyze
+            (str) month - name of the month to filter by, or "all" to apply no month filter
+            (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        Returns:
+            df - Pandas DataFrame containing city data filtered by month and day
+        """
     df = pd.read_csv(CITY_DATA[city])
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     df['hour'] = df['Start Time'].dt.hour
